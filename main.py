923605f133fcf7141ea621e75f9c78ee8fa96873
Kamil Khabibullin <vnj644@gmail.com>
2022-09-12 00:21:55 +0300

    Fixed

--- main.py
+++ main.py
@@ -1,18 +1,17 @@
 import requests
 import json
 
+
 def pars():
     result = []
-    r = requests.get('https://rickandmortyapi.com/api/character')
-    jsonData = r.json()['results']
-    for person in jsonData:
-        for episodes in person['episode']:
-            sp = episodes.split('/')[-1]
-            numbers = ['30', '31', '32', '33']
-            if sp in numbers:
-                result.append(person['image'])
+    for i in range(30, 34):
+        r = requests.get(f'https://rickandmortyapi.com/api/character/?page={i}')
+        jsonData = r.json()['results']
+        for person in jsonData:
+            result.append(person['image'])
 
     return list(set(result))
+
 
 if __name__ == '__main__':
     with open('big_data.json', 'w') as file:
