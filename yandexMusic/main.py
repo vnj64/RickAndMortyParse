14cc2ec90638e707b9528dd88d24250798bd4367
Kamil Khabibullin <vnj644@gmail.com>
2022-09-14 00:24:11 +0300

    executable file

---
+++ yandexMusic/main.py
@@ -0,0 +1,32 @@
+import requests
+import json
+
+
+def parse(url):
+    r = requests.get(url)
+    jsonData = r.json()['chartPositions']
+    result = {}
+    
+
+    for data in jsonData:
+        position = data["chartPosition"]["position"]
+        artists = [art["name"] for art in data["track"]["artists"]]
+        name = data["track"]["title"]
+
+        result[position] = {
+            "name": name,
+            "artists": artists
+        }
+
+    return result
+
+
+
+
+
+if __name__ == "__main__":
+    YANDEX_URL = 'https://music.yandex.ru/handlers/main.jsx?what=chart&lang=ru&external-domain=music.yandex.ru&overembed=false'
+
+    with open("charts.json", "w") as file:
+        json.dump(parse(YANDEX_URL), file, ensure_ascii=False)
+    file.close()
