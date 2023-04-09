529fa031841f91bc2243068b5932d736f6abbee2
Kamil Khabibullin <kamil_khabibullin@bk.ru>
2023-04-09 22:16:02 +0300

    updated

--- invokerGame/main.py
+++ invokerGame/main.py
@@ -5,54 +5,40 @@ import time
 
 DRIVER = webdriver.Chrome(executable_path="/home/sirius/parsers/all parsers/invokerGame/chromedriver/chromedriver")
 
-
 def click_invoker(url):
-    try:
-        DRIVER.get(url=url)
+    DRIVER.get(url=url)
 
-        survival_button = DRIVER.find_element('xpath', '//*[@id="GameMenu"]/div/table/tbody/tr[2]/td[1]/input')
-        survival_button.click()
-
-        without_record = DRIVER.find_element('xpath', '//*[@id="LoginNotification"]/div/table/tbody/tr/td[3]/a')
-        without_record.click()
-
-        start_game = DRIVER.find_element('xpath', '//*[@id="GameInterface"]/div[1]/nobr/table/tbody/tr/td/input')
-        start_game.click()
-
-        spell_quas = DRIVER.find_element('xpath', '//*[@id="GameInterface"]/div[2]/div[1]')
-        spell_wex = DRIVER.find_element('xpath', '//*[@id="GameInterface"]/div[2]/div[2]')
-        spell_exort = DRIVER.find_element('xpath', '//*[@id="GameInterface"]/div[2]/div[3]')
-        spell_one = DRIVER.find_element('xpath', '//*[@id="GameInterface"]/div[2]/div[4]')
-        spell_two = DRIVER.find_element('xpath', '//*[@id="GameInterface"]/div[2]/div[5]')
-        spell_ready = DRIVER.find_element('xpath', '//*[@id="GameInterface"]/div[2]/div[6]')
+    survival_button = DRIVER.find_element('xpath', '/html/body/form/div[3]/div[6]/div[2]/div[2]/div/table/tbody/tr[2]/td[1]/input')
+    survival_button.click()
 
 
-        time.sleep(1)
+    start_game = DRIVER.find_element('xpath', '/html/body/form/div[3]/div[6]/div[2]/div[3]/div[1]/nobr/table/tbody/tr/td/input')
+    start_game.click()
 
-        while True:
-            cold_snap(spell_quas, spell_ready, spell_one)
-            ghost_walk(spell_quas, spell_wex, spell_ready, spell_one)
-            ice_wall(spell_quas, spell_exort, spell_ready, spell_one)
-            emp(spell_wex, spell_ready, spell_one)
-            tornado(spell_wex, spell_quas, spell_ready, spell_one)
-            alacrity(spell_wex, spell_exort, spell_ready, spell_one)
-            sun_strike(spell_exort, spell_ready, spell_one)
-            forge_spirit(spell_exort, spell_quas, spell_ready, spell_one)
-            chaos_meteor(spell_exort, spell_wex, spell_ready, spell_one)
-            blast(spell_exort, spell_quas, spell_wex, spell_ready, spell_one)
+    spell_quas = DRIVER.find_element('xpath', '//*[@id="GameInterface"]/div[2]/div[1]')
+    spell_wex = DRIVER.find_element('xpath', '//*[@id="GameInterface"]/div[2]/div[2]')
+    spell_exort = DRIVER.find_element('xpath', '//*[@id="GameInterface"]/div[2]/div[3]')
+    spell_one = DRIVER.find_element('xpath', '//*[@id="GameInterface"]/div[2]/div[4]')
+    spell_two = DRIVER.find_element('xpath', '//*[@id="GameInterface"]/div[2]/div[5]')
+    spell_ready = DRIVER.find_element('xpath', '//*[@id="GameInterface"]/div[2]/div[6]')
+
+
+    time.sleep(1)
+
+    while True:
+        cold_snap(spell_quas, spell_ready, spell_one)
+        ghost_walk(spell_quas, spell_wex, spell_ready, spell_one)
+        ice_wall(spell_quas, spell_exort, spell_ready, spell_one)
+        emp(spell_wex, spell_ready, spell_one)
+        tornado(spell_wex, spell_quas, spell_ready, spell_one)
+        alacrity(spell_wex, spell_exort, spell_ready, spell_one)
+        sun_strike(spell_exort, spell_ready, spell_one)
+        forge_spirit(spell_exort, spell_quas, spell_ready, spell_one)
+        chaos_meteor(spell_exort, spell_wex, spell_ready, spell_one)
+        blast(spell_exort, spell_quas, spell_wex, spell_ready, spell_one)
 
 
 
-
-    except Exception as e:
-        print(e)
-
-    finally:
-
-        time.sleep(10)
-
-        DRIVER.close()
-        DRIVER.quit()
 
 def cold_snap(spell_quas, spell_ready, spell_one):
     for i in range(3):
@@ -60,12 +46,16 @@ def cold_snap(spell_quas, spell_ready, spell_one):
     spell_ready.click()
     spell_one.click()
 
+
+
 def ghost_walk(spell_quas, spell_wex, spell_ready, spell_one):
     for i in range(2):
         spell_quas.click()
     spell_wex.click()
     spell_ready.click()
     spell_one.click()
+
+
 
 def ice_wall(spell_quas, spell_exort, spell_ready, spell_one):
     for i in range(2):
@@ -75,12 +65,16 @@ def ice_wall(spell_quas, spell_exort, spell_ready, spell_one):
     spell_ready.click()
     spell_one.click()
 
+
+
 def emp(spell_wex, spell_ready, spell_one):
     for i in range(3):
         spell_wex.click()
 
     spell_ready.click()
     spell_one.click()
+
+
 
 def tornado(spell_wex, spell_quas, spell_ready, spell_one):
     for i in range(2):
@@ -90,6 +84,8 @@ def tornado(spell_wex, spell_quas, spell_ready, spell_one):
     spell_ready.click()
     spell_one.click()
 
+
+
 def alacrity(spell_wex, spell_exort, spell_ready, spell_one):
     for i in range(2):
         spell_wex.click()
@@ -98,12 +94,17 @@ def alacrity(spell_wex, spell_exort, spell_ready, spell_one):
     spell_ready.click()
     spell_one.click()
 
+
+
+
 def sun_strike(spell_exort, spell_ready, spell_one):
     for i in range(3):
         spell_exort.click()
 
     spell_ready.click()
     spell_one.click()
+
+
 
 def forge_spirit(spell_exort, spell_quas, spell_ready, spell_one):
     for i in range(2):
@@ -113,6 +114,8 @@ def forge_spirit(spell_exort, spell_quas, spell_ready, spell_one):
     spell_ready.click()
     spell_one.click()
 
+
+
 def chaos_meteor(spell_exort, spell_wex, spell_ready, spell_one):
     for i in range(2):
         spell_exort.click()
@@ -120,6 +123,8 @@ def chaos_meteor(spell_exort, spell_wex, spell_ready, spell_one):
     spell_wex.click()
     spell_ready.click()
     spell_one.click()
+
+
 
 def blast(spell_exort, spell_quas, spell_wex, spell_ready, spell_one):
     spell_quas.click()
